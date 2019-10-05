fe359a83e8eaa99ca7f52f6269edc0c0f5ded878
PhilDK1 <philipdkrause@gmail.com>
2019-10-05 09:33:32 +0100

    ..3_4.cxx started

---
+++ problem set 3/Krause_Philip_3_4.cxx
@@ -0,0 +1,30 @@
+/*
+ * Philip Krause
+ * 
+ * 118470776
+ *
+ * PY2105
+ *
+ *
+ * File used to approximate the velocity using the Euler Method of numerical differentitation
+ * and plots the velocity at a given time t on the interval [0, T]. Given that x(t) = x(0) + v(0)*t + 0.5*a*t^2
+ * where the velocity is constant so the above equation goes to x(t) = x(0) + v*t
+ * 
+ *
+ */
+
+
+//header files 
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+// namespace set up to prevent the need to type "std::" in each place where it would normally be required
+using namespace std;
+
+//Main Function
+int main(int argc, char **argv) {
+
+}
