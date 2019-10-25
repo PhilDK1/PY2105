0ad201f2241a09747cd8640b4a1d8717ebfbe124
PhilDK1 <philipdkrause@gmail.com>
2019-10-25 19:21:56 +0100

    initial commit

---
+++ Project/main.cxx
@@ -0,0 +1,62 @@
+/*
+    Philip Krause
+    118470776@umail.ucc.ie
+    Project 9 Quantum Assignment
+*/
+
+
+// Header Files
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <cmath>
+using namespace std;
+#include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+#include "gnuplot.cxx"
+
+//Main files
+int main() {
+
+    //define variables
+    int N = 1000;
+
+    double phi[N];
+
+    double E;
+    double V[N];
+    double L = 1;
+
+    double phi_0;
+    double phi_1;
+
+    double offset = 1;
+    double V_at_L = 100;
+
+    double graphing_distance = 2*(L+offset);
+    double del_x = graphing_distance/N;
+
+    double x = -0.5*graphing_distance;
+    double current_x;
+    for (int i = 0; i < N; i++) {
+        if (x < L) {
+            V[i] = V_at_L;
+        } else if (x > L){
+            V[i] = V_at_L;
+            
+        } else {
+            V[i] = 0;
+        }
+
+        x += del_x;
+    }
+    cout << abs(-x) << endl;
+    cout << abs(x) << endl;
+    cout << -x << endl;
+    cout << x << endl;
+    
+    return 0;
+}
