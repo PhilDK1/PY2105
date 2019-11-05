a7fe6390589eecabe1e8431925ec3be34ddc16bf
PhilDK1 <philipdkrause@gmail.com>
2019-11-05 15:17:24 +0000

    adjustments (not working correctly)

--- Project/wave.cxx
+++ Project/wave.cxx
@@ -16,8 +16,8 @@ using namespace std;
 #include <stdlib.h>
 #include <stdarg.h>
 #include <assert.h>
-#include "gnuplot.cxx"
-#include "potential_fn.cxx"
+// #include "gnuplot.cxx"
+// #include "potential_fn.cxx"
 
 //calculate fn
 double* calc(double *V, 
@@ -124,6 +124,88 @@ double *gen_phi_even(double *wavefn,
 
     for (int i = 2; i < mid+2; i++) {
         phi_temp = 2*wavefn[mid - i + 1] - wavefn[mid - i + 2] - 2*del_x*del_x*(E - V[mid - i + 1])*wavefn[mid - i + 1];
+        if (abs(phi_temp) < cutoff) {
+            wavefn[mid - i] = phi_temp;
+        } else {
+            if (phi_temp > 0) {
+                // divering up
+                for (int n = 0; n < (mid - i); n++)
+                {
+                    wavefn[mid - i - n] = 2.5;
+                }
+                break;
+            } else if (phi_temp <  0) {
+                // divering down
+                for (int n = 0; n < ( mid -i); n++)
+                {
+                wavefn[mid - i - n] = -2.5;
+                }
+                break;
+            }
+        }
+        
+    }
+
+
+
+    return wavefn;
+}
+
+
+
+double *gen_phi_odd(double *wavefn,
+                double *V,
+                double *X,
+                int size,
+                double phi_0,
+                double phi_n1,
+                double graphing_distance, 
+                double E,
+                double cutoff) {
+    
+
+    double del_x = graphing_distance/size;
+    double phi_temp;
+
+    int mid;
+    if (size % 2 == 0) {
+        mid = (size/2);
+    } else {
+        mid = size -1;
+        mid = (mid/2);
+    }
+    
+    wavefn[mid] = 2*phi_0 - phi_n1 - 2*del_x*del_x*(E - V[mid-1])*phi_0;
+    wavefn[mid + 1] = 2*wavefn[mid] - phi_0 - 2*del_x*del_x*(E - V[mid-1])*wavefn[mid];
+    wavefn[mid - 1] = -2*wavefn[mid] + phi_0 + 2*del_x*del_x*(E - V[mid-1])*wavefn[mid];
+    for (int i = 2; i < mid; i++) {
+        phi_temp = 2*wavefn[mid + i - 1] - wavefn[mid + i - 2] - 2*del_x*del_x*(E - V[mid + i - 1])*wavefn[mid + i - 1];
+        if (abs(phi_temp) < cutoff) {
+            wavefn[mid + i] = phi_temp;
+        } else {
+            if (phi_temp > 0) {
+                // divering up
+                for (int n = 0; n < (mid - i); n++)
+                {
+                wavefn[mid + i + n] = 2.5;
+                }
+                break;
+            } else if (phi_temp <  0) {
+                // divering down
+                for (int n = 0; n < (mid - i); n++)
+                {
+                wavefn[mid + i + n] = -2.5;
+                }
+                break;
+            }
+            
+            
+        }
+        
+    }
+
+    for (int i = 2; i < mid+2; i++) {
+        phi_temp = -2*wavefn[mid - i + 1] + wavefn[mid - i + 2] + 2*del_x*del_x*(E - V[mid - i + 1])*wavefn[mid - i + 1];
         if (abs(phi_temp) < cutoff) {
             wavefn[mid - i] = phi_temp;
         } else {
