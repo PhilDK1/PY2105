9fc7ace5f3d92bc0d883a9a5c27acae686bdb91b
PhilDK1 <philipdkrause@gmail.com>
2019-11-05 15:18:48 +0000

    file updated

--- Project/main.cxx
+++ Project/main.cxx
@@ -97,12 +97,66 @@ const double sq_pi = pi * pi;
 
 
 // Main fn
+// int main() {
+//     int N = 50000;
+//     double V[N], X[N], wavefn[N];
+//     double graphing_distance = 5;
+//     double E[1];
+//     E[0]= 11;
+//     double del_E[1];
+//     del_E[0] = 0.5;
+//     double cutoff = 2.5;
+//     double lVstep = 1000;
+//     double rVstep = 1000;
+//     double VStepPoint_l = -1;
+//     double VstepPoint_r = 1;
+//     double last_diverge[1];
+//     last_diverge[0] = 0;
+//     double del_x = graphing_distance/N;
+
+//     gen_v(V, X, N, lVstep, rVstep, graphing_distance, VStepPoint_l, VstepPoint_r);
+//     gen_phi_even(wavefn, V, X, N, 1, 1, graphing_distance, E[0], cutoff);
+
+//     bool cont = true;
+//     int count = 0;
+//     char prompt;
+//     while (cont) {
+//         calc(V, X, wavefn, N, last_diverge, graphing_distance, E, del_E, cutoff);
+//         cout << "E is: " << E[0] << endl;
+//         cout << "del E is: " << del_E[0] << endl;
+//         gen_phi_even(wavefn, V, X, N, 1, 1, graphing_distance, E[0], cutoff);
+//         if (count % 50 == 0) {
+//             gnuplot_one_function("Wave Function", "linespoints", "X", "phi", X, wavefn, N);
+//             cout << "Continue: ";
+        
+//             cin >> prompt;
+//             switch (prompt) {
+//                 case 'n' :
+//                 case 'N' :
+//                     cont = false;
+//             }
+//         }
+//         count++;
+
+//     }
+//     char filename[30];
+//     char title[35];
+
+//     sprintf(filename, "plot of wavefn E=%.5lf.jpg", E[0]);
+//     sprintf(title, "Wave Function with Energy = %.5lf", E[0]);
+//     gnuplot_one_function_jpg(title, "linespoints", "X", "phi", X, wavefn, N, filename);
+
+
+//     return 0;
+// }
+
+
 int main() {
     int N = 50000;
     double V[N], X[N], wavefn[N];
     double graphing_distance = 5;
     double E[1];
-    E[0]= 11;
+    E[0]= 1;
     double del_E[1];
     del_E[0] = 0.5;
     double cutoff = 2.5;
@@ -115,6 +169,7 @@ int main() {
     double del_x = graphing_distance/N;
 
     gen_v(V, X, N, lVstep, rVstep, graphing_distance, VStepPoint_l, VstepPoint_r);
+    // gen_phi_odd(wavefn, V, X, N, 0, -del_x, graphing_distance, E[0], cutoff);
     gen_phi_even(wavefn, V, X, N, 1, 1, graphing_distance, E[0], cutoff);
 
     bool cont = true;
@@ -124,7 +179,7 @@ int main() {
         calc(V, X, wavefn, N, last_diverge, graphing_distance, E, del_E, cutoff);
         cout << "E is: " << E[0] << endl;
         cout << "del E is: " << del_E[0] << endl;
-        gen_phi_even(wavefn, V, X, N, 1, 1, graphing_distance, E[0], cutoff);
+        gen_phi_odd(wavefn, V, X, N, 1, 1, graphing_distance, E[0], cutoff);
         if (count % 50 == 0) {
             gnuplot_one_function("Wave Function", "linespoints", "X", "phi", X, wavefn, N);
             cout << "Continue: ";
