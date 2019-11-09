4e728b4f8aa5e53536f8ff5dd4c255662eb91d35
PhilDK1 <philipdkrause@gmail.com>
2019-11-09 17:06:42 +0000

    mathematical functions completed

--- Project/wave_maths.cxx
+++ Project/wave_maths.cxx
@@ -11,12 +11,20 @@ using namespace std;
 #include <assert.h>
 
 
+
+// double * gen_test(double *Y, int size) {
+//     for (int i = 0; i < size; i++){
+//         Y[i] = i*i + 2i + 1
+//     }
+// }
+
 double* square(
     double *wavefn,
     double *squared_wf,
     int size) {
+    
 
-    for (int i = 0; i++ ; i < size) {
+    for (int i = 0; i < size; i++ ) {
         squared_wf[i]= wavefn[i]*wavefn[i];
     }
 
@@ -33,14 +41,15 @@ double integrate(
     int count = 0;
     int upper = 0;
     // double Sum = 0;
-    while (count < size || X[count] < left_L) {
+    while (X[count] < left_L) {
         count++;
     }
     count++;
 
-    while (upper < size || X[upper] < right_L) {
+    while (X[upper] < right_L) {
         upper++;
     }
+    upper--;
 
     double Sum = 0;
     double ans;
@@ -61,3 +70,54 @@ double integrate(
 
 
 }
+
+double *Normalise(double *wavefn, double area, double size) {
+    for (int i = 0; i < size; i++) {
+        wavefn[i] = (1/(sqrt(area)))*wavefn[i];
+    }
+    return wavefn;
+}
+
+
+/*
+int main() {
+    int N = 50000;
+    double V[N], X[N], wavefn[N], dupl[N];
+    double graphing_distance = 5;
+    double E[1];
+    E[0]= 4;
+    double del_E[1];
+    del_E[0] = 1;
+    double cutoff = 2.5;
+    double lVstep = 1000;
+    double rVstep = 1000;
+    double VStepPoint_l = -1;
+    double VstepPoint_r = 1;
+    double last_diverge[1];
+    last_diverge[0] = 0;
+    double del_x = graphing_distance/N;
+    double squared_function[N];
+    double ans;
+
+    gen_v(V, X, N, lVstep, rVstep, graphing_distance, VStepPoint_l, VstepPoint_r);
+    gen_phi_odd(wavefn, V, X, N, 0, -del_x, graphing_distance, E[0], cutoff);
+    gen_phi_odd(dupl, V, X, N, 0, -del_x, graphing_distance, E[0], cutoff);
+
+    square(wavefn, squared_function, N);
+    ans = integrate(squared_function, X, VStepPoint_l, VstepPoint_r, N, del_x);
+
+    cout << ans << endl;
+
+    Normalise(dupl, ans, N);
+    square(dupl, squared_function, N);
+    ans = integrate(squared_function, X, VStepPoint_l, VstepPoint_r, N, del_x);
+
+    cout << ans << endl;
+    gnuplot_two_functions("wf & sqwf", "linespoints", "X", "value", X, wavefn, N, "yurt", X, dupl, N, "norm");
+
+
+
+
+    return 0;
+}
+*/
