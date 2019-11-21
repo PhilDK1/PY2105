5e7c5b1d8f2c1633d4accc13ca0d8ef2062662ba
PhilDK1 <philipdkrause@gmail.com>
2019-11-21 08:25:54 +0000

    matching method implimented w/ transfer to new arr

--- Project/Philip_Krause_Project_f.cxx
+++ Project/Philip_Krause_Project_f.cxx
@@ -48,7 +48,7 @@ double *gen_phi_left(double *wavefn, double *V, double *X, int N, double E, doub
 
     for (int i = 0; i < N- ind; i++) {
         wavefn[ind +i] = 2*wavefn[ind +i -1] -wavefn[ind + i -2] - 2*del_x*del_x*(E- V[ind +i -1])*wavefn[ind +i -1];
-        cout << i << "      " << wavefn[ind +i] << "    " << (wavefn[ind +i] - wavefn[ind +i -1]) << "      " << V[ind +i -1] << endl;
+        // cout << i << "      " << wavefn[ind +i] << "    " << (wavefn[ind +i] - wavefn[ind +i -1]) << "      " << V[ind +i -1] << endl;
     }
 
 
@@ -73,7 +73,7 @@ double *gen_phi_right(double *wavefn, double *V, double *X, int N, double E, dou
 
 
 
-int min(double *arr, int N){
+int locate_min(double *arr, int N){
     int min = 0;
     double min_val = arr[0];
     for (int i = 0; i < N; i++) {
@@ -93,6 +93,84 @@ double *scale(double *wavefn, double factor, int N){
     return wavefn;
 }
 
+double finite_derive(double *Y, double *X, int index) {
+    double del_x = abs(X[index] -  X[index + 1]);
+    double deriv = (Y[index+1]- Y[index])/del_x;
+    return deriv;
+}
+
+double *adjust(double *V, double *X, double *wavefn_1, double *wavefn_2, double *wave_function, int N, double init_E, double inc_E, double x_l, double x_r, double start, double end) {
+    // find minium potential of on the range
+    int minimum = locate_min(V, N);
+    double tolerance = ((end - start)/N)*((end - start)/N)*((end - start)/N)*((end - start)/N);
+    cout << tolerance << endl;
+    gen_phi_left(wavefn_1, V, X, N, init_E, x_l, start, end);
+    gen_phi_right(wavefn_2, V, X, N, init_E, x_r, start, end);
+    // gnuplot_two_functions("test plot", "lines", "X", "phi", X, wavefn_1, N, "left", X, wavefn_2, N, "right");
+    double E = init_E;
+    double ratio = (wavefn_1[minimum]/wavefn_2[minimum]);
+    cout << ratio<< endl;
+    scale(wavefn_2, ratio, N);
+    gnuplot_two_functions("test plot", "lines", "X", "phi", X, wavefn_1, N, "left", X, wavefn_2, N, "right");
+    int match = 1;
+    double diff = finite_derive(wavefn_1, X, minimum) - finite_derive(wavefn_2, X, minimum); // >0 then d_phi_l/dx > d_phi_r/dx else l<r
+    cout << diff << endl;
+
+    int count = 1;
+    int ans;
+    if (diff >0 ) {
+        match = 1;
+
+    }else if (diff <0) {
+        match = -1;
+    }
+
+    while (abs(diff) > tolerance) {
+        cout << count << endl;
+        if ((match == 1) && (diff >0)){
+            // energy needs to increase
+            // cout << "1   1  " << E << endl;
+            E = E + inc_E;
+            // cout << E << endl;
+        } else if ((match == 1) && (diff <0)) {
+            // increased last time so needs to decrease
+            inc_E  = inc_E/2;
+            // cout << "1  -1  " << E << endl;
+            E = E - inc_E;
+            // cout << E << endl;
+            match = -1;
+        } else if ((match == -1) && (diff >0)) {
+            // decreased last time needs to increase
+            inc_E  = inc_E/2;
+            // cout << "-1  1  " << E << endl;
+            E = E + inc_E;
+            // cout << E << endl;
+            match = 1;
+        } else if ((match == -1) && (diff <0)) {
+            // energy needs to decrease
+            // cout << "-1 -1  " << E << endl;
+            E = E - inc_E;
+            // cout << E << endl;
+        }
+        gen_phi_left(wavefn_1, V, X, N, E, x_l, start, end);
+        gen_phi_right(wavefn_2, V, X, N, E, x_r, start, end);
+        ratio = (wavefn_1[minimum]/wavefn_2[minimum]);
+        scale(wavefn_2, ratio, N);
+        diff = finite_derive(wavefn_1, X, minimum) - finite_derive(wavefn_2, X, minimum); // >0 then d_phi_l/dx > d_phi_r/dx else l<r
+        count++;
+        // gnuplot_two_functions("test plot", "lines", "X", "phi", X, wavefn_1, N, "left", X, wavefn_2, N, "right");
+        // cin >> ans;
+    }
+    for (int i = 0; i < N; i++) {
+        if (i <= minimum) {
+            wave_function[i] = wavefn_1[i];
+        } else {
+            wave_function[i] = wavefn_2[i];
+        }
+    }
+
+}
+
 int main() {
     int N = 1000;
     double end = 3;
@@ -102,7 +180,7 @@ int main() {
     double point = 1;
     double x_0 = 0.9;
 
-    double V[N], X[N], wavefn[N], wavfn[N];
+    double V[N], X[N], wavefn[N], wavfn[N], wave_fn[N];
     double x_r = end - 0.05;
     double x_l = x_0 + 0.05;
     // cout << indexing_right(X, x_r, N);
@@ -110,11 +188,11 @@ int main() {
     // double x_0 = -point - 0.1;
     // double x_f = point + 0.1;
     double sig =1.0;
-    double ep = 10;
+    double ep = 250;
 
     LJV(V, X, x_0, end, N, sig, ep);
-    cout << indexing_right(X, x_r, N) << endl;
-    cout << indexing_left(X, x_l, N) << endl;
+    // cout << indexing_right(X, x_r, N) << endl;
+    // cout << indexing_left(X, x_l, N) << endl;
 
     // mini_step(V, X, N, Vstep, end, point, mVstep, m_l);
     // for (int i = 0; i < N; i++) {
@@ -123,14 +201,20 @@ int main() {
     // }
 
 
-    gen_phi_right(wavefn, V, X, N, -1.6, x_r, x_0, end);
-    gen_phi_left(wavfn, V, X, N, -1.6, x_l, x_0, end);
-    scale(wavefn, 0.5, N);
-    scale(wavfn, 5, N);
+    // gen_phi_right(wavefn, V, X, N, -1.6, x_r, x_0, end);
+    // gen_phi_left(wavfn, V, X, N, -1.6, x_l, x_0, end);
+    // scale(wavefn, 0.5, N);
+    // scale(wavfn, 5, N);
 
+    adjust(V, X, wavefn, wavfn, wave_fn, N, -1.6, 0.05, x_l, x_r, x_0, end);
+
+
+    gnuplot_two_functions("test plot", "lines", "X", "phi", X, wavfn, N, "left start", X, wavefn, N, "right start");
+    // adjust(V, X, wavefn, wavfn, N, -1.2039, 0.5, x_l, x_r, x_0, end);
     // gnuplot_two_functions("test plot", "lines", "X", "phi", X, wavfn, N, "wav", X, wavefn, N, "pot");
-    gnuplot_two_functions_jpg("Wave function for non symmetric potential", "linespoints", "X axis", "Wavefunction Amplitude", X, wavefn, N, "wavefunction (right)", X, wavfn, N, "wavefunction (left)", "Lennard_Jones_Potential_Wavefunction.jpg");
-    // gnuplot_one_function("test", "lines", "X", "phi", X, wavefn, N);
+
+    // gnuplot_two_functions_jpg("Wave function for non symmetric potential", "lines", "X axis", "Wavefunction Amplitude", X, wavefn, N, "wavefunction (right)", X, wavfn, N, "wavefunction (left)", "Lennard_Jones_Potential_Wavefunction.jpg");
+    gnuplot_one_function("test", "lines", "X", "phi", X, wave_fn, N);
 
 
     return 0;
